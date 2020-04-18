2811622ba57d237ae115be48c248c913f6a54294
Libni Andrea Hernández Rosales <201900040@colsanjose.edu.gt>
2020-04-17 22:37:10 -0600

    TODO

--- calcular.php
+++ calcular.php
@@ -1,34 +1,34 @@
 <?php
-$valor1 = $_POST ['n1'] 
-$valor2 = $_POST ['n2']
-$suma= $_POST ['suma'] 
-$resta= $_POST ['resta'] 
-$multi= $_POST ['multi'] 
+$valor1= $_POST ['n1'];
+$valor2= $_POST ['n2'];
+$suma= $_POST ['suma'];
+$resta= $_POST ['resta']; 
+$multi= $_POST ['multi'];
 $div= $_POST ['div'] 
 {
-        if ($operador == $suma)
+        mas si ($operador == $suma_POST)
 	{
 			echo "La suma es: "
-			$total=$valor1_POST + $valor2_POST;
+			$total=$valor1 + $valor2;
 			
 	} 
-	 mas si ($operador == $resta)
+	 mas si ($operador == $resta_POST)
 	{
 			echo "La resta es: "
-			$total=$valor1_POST - $valor2_POST;
+			$total=$valor1 - $valor2;
 			
 	} 
-	mas si ($operador == $multi) 
+	mas si ($operador == $multi_POST) 
 	{
 			echo "La multiplicacion es: "
-			$total=$valor1_POST * $valor2_POST;
+			$total=$valor1 * $valor2;
 			
 		
 	} 
-	mas ($operador == $div) 
+	mas ($operador == $div_POST) 
 	{
 			echo "La division es: "
-			$total=$valor1_POST / $valor2_POST;
+			$total=$valor1 / $valor2;
 			
 	}
 	
