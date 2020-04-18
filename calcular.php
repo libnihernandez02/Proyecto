3119a23eee9ea3d20a254964a4546dd3c3c92c13
Libni Andrea Hernández Rosales <201900040@colsanjose.edu.gt>
2020-04-17 21:17:56 -0600

    Diseño

--- calcular.php
+++ calcular.php
@@ -5,26 +5,26 @@ $valor2_POST ['n2']
         if ($_POST["operador"] == "suma")
 	{
 			echo "La suma es: "
-			$total=$valor1_POST+$valor2_POST;
+			$total=$valor1_POST + $valor2_POST;
 			
 	} 
 	elseif ($_POST["operador"] == "resta")
 	{
 			echo "La resta es: "
-			$total=$valor1_POST-$valor2_POST
+			$total=$valor1_POST - $valor2_POST;
 			
 	} 
 	else if ($_POST["operador"] == "multiplicacion") 
 	{
 			echo "La multiplicacion es: "
-			$total=$valor1_POST*$valor2_POST;
+			$total=$valor1_POST * $valor2_POST;
 			
 		
 	} 
 	else if ($_POST["operador"] == "division") 
 	{
 			echo "La division es: "
-			$total=$valor1_POST/$valor2_POST;	
+			$total=$valor1_POST / $valor2_POST;
 			
 	}
 	
