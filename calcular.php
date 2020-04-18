2a58d2b842016a1d6ddf7b3f462ec69cab3aec95
ethan10rodriguez <62311552+ethan10rodriguez@users.noreply.github.com>
2020-04-17 23:04:52 -0600

    Update calcular.php

--- calcular.php
+++ calcular.php
@@ -3,26 +3,31 @@
 $valor1= $_POST ['n1'];
 $valor2= $_POST ['n2'];
 $operador=$_POST['Operador'];
+$Total;
 {
         if($operador == suma)
 	{
-			echo "La suma es: " . ($valor1 + $valor2);
+			echo "La suma es: ".$Total;
+				$total = $valor1 + $valor2;
 			
 	} 
 	 if($operador == resta)
 	{
-			echo "La resta es: " . ($valor1 - $valor2);
+			echo "La resta es: ".$Total;
+				$Total = $valor1 - $valor2;
 			
 	} 
 	 if($operador == multi) 
 	{
-			echo "La multiplicacion es: " . ($valor1 * $valor2);
+			echo "La multiplicacion es: ".$Total; 
+				$Total = $valor1 * $valor2;
 			
 		
 	} 
 	 if($operador == div) 
 	{
-			echo "La division es: " . ($valor1 / $valor2);
+			echo "La division es: ".$Total; 
+				$Total = $valor1 / $valor2;
 			
 	}
 	
