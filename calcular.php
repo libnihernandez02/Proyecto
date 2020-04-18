8a961efb8b9c52ff7a3a0df30c1333ff84270b4a
Libni Andrea Hernández Rosales <201900040@colsanjose.edu.gt>
2020-04-17 20:17:29 -0600

    Agregue texto

--- calcular.php
+++ calcular.php
@@ -1,25 +1,30 @@
 <?php
-if ($_POST ["valor1"] !="" and $_POST ["valor2"]!="")
+if ($_POST ["n1"] !="" and $_POST ["n2"]!="")
 {
         if ($_POST["operador"] == "suma")
 	{
-			print ($resultado = $_POST ["valor1"] + $_POST ["valor2"]);
-			print ('<br /><a href="calculadora.php">Volver</a>');
+			echo "La suma es: "
+			print ($resultado = $_POST ["nr1"] + $_POST ["n2"]);
+			print ('<br /><a href="calcular.php">');
 	} 
 	elseif ($_POST["operador"] == "resta")
 	{
-			print ($resultado = $_POST ["valor1"] - $_POST ["valor2"]);
-			print ('<br /><a href="calculadora.php">Volver</a>');
+			echo "La resta es: "
+			print ($resultado = $_POST ["n1"] - $_POST ["n2"]);
+			print ('<br /><a href="calcular.php">');
 	} 
-	elseif ($_POST["operador"] == "multiplicacion") 
+	else if ($_POST["operador"] == "multiplicacion") 
 	{
-			print ($resultado = $_POST ["valor1"] * $_POST ["valor2"]);
-			print ('<br /><a href="calculadora.php">Volver</a>');
+			echo "La multiplicacion es: "
+			print ($resultado = $_POST ["n1"] * $_POST ["n2"]);
+			print ('<br /><a href="calcular.php">');
 		
 	} 
-	elseif ($_POST["operador"] == "division") 
+	else if ($_POST["operador"] == "division") 
 	{
-			print ($resultado = $_POST ["valor1"] / $_POST ["valor2"]);	
+			echo "La division es: "
+			print ($resultado = $_POST ["n1"] / $_POST ["n2"]);	
+			print ('<br /><a href="calcular.php">');
 	}
 	
 		
