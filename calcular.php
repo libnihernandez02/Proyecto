75646ec90f079c7e323b6860aafdad4b995148ec
Libni Andrea Hernández Rosales <201900040@colsanjose.edu.gt>
2020-04-17 22:21:56 -0600

    Form Calc

--- calcular.php
+++ calcular.php
@@ -1,27 +1,31 @@
 <?php
-$valor1_POST ['n1'] 
-$valor2_POST ['n2']
+$valor1 = $_POST ['n1'] 
+$valor2 = $_POST ['n2']
+$suma= $_POST ['suma'] 
+$resta= $_POST ['resta'] 
+$multi= $_POST ['multi'] 
+$div= $_POST ['div'] 
 {
-        if ($_POST["operador"] == "suma")
+        if ($operador == $suma)
 	{
 			echo "La suma es: "
 			$total=$valor1_POST + $valor2_POST;
 			
 	} 
-	elseif ($_POST["operador"] == "resta")
+	 mas si ($operador == $resta)
 	{
 			echo "La resta es: "
 			$total=$valor1_POST - $valor2_POST;
 			
 	} 
-	else if ($_POST["operador"] == "multiplicacion") 
+	mas si ($operador == $multi) 
 	{
 			echo "La multiplicacion es: "
 			$total=$valor1_POST * $valor2_POST;
 			
 		
 	} 
-	else if ($_POST["operador"] == "division") 
+	mas ($operador == $div) 
 	{
 			echo "La division es: "
 			$total=$valor1_POST / $valor2_POST;
