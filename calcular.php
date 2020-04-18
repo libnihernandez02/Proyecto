a7a420547821937cc025c91b79b1e7672f0c7c21
Libni Andrea Hernández Rosales <201900040@colsanjose.edu.gt>
2020-04-17 20:47:39 -0600

    Cambio en formula

--- calcular.php
+++ calcular.php
@@ -1,30 +1,31 @@
 <?php
-if ($_POST ["n1"] !="" and $_POST ["n2"]!="")
+$valor1_POST ['n1'] 
+$valor2_POST ['n2']
 {
         if ($_POST["operador"] == "suma")
 	{
 			echo "La suma es: "
-			print ($resultado = $_POST ["nr1"] + $_POST ["n2"]);
-			print ('<br /><a href="calcular.php">');
+			$total=$valor1_POST+$valor2_POST;
+			
 	} 
 	elseif ($_POST["operador"] == "resta")
 	{
 			echo "La resta es: "
-			print ($resultado = $_POST ["n1"] - $_POST ["n2"]);
-			print ('<br /><a href="calcular.php">');
+			$total=$valor1_POST-$valor2_POST
+			
 	} 
 	else if ($_POST["operador"] == "multiplicacion") 
 	{
 			echo "La multiplicacion es: "
-			print ($resultado = $_POST ["n1"] * $_POST ["n2"]);
-			print ('<br /><a href="calcular.php">');
+			$total=$valor1_POST*$valor2_POST;
+			
 		
 	} 
 	else if ($_POST["operador"] == "division") 
 	{
 			echo "La division es: "
-			print ($resultado = $_POST ["n1"] / $_POST ["n2"]);	
-			print ('<br /><a href="calcular.php">');
+			$total=$valor1_POST/$valor2_POST;	
+			
 	}
 	
 		
